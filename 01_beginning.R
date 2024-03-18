8f087c0e75b5381cf1753954fd7e7ea68c28cee8
Filippo Paganelli <162352335+FiloPaga@users.noreply.github.com>
2024-03-18 11:58:15 +0100

    Update 01_beginning.R

--- 01_beginning.R
+++ 01_beginning.R
@@ -1,12 +1,12 @@
 # First R script
 
 # R as calculator 
-a <- 6*7
+a <- 6*7 # semplici operazioni assegnate ad un oggetto
 b <- 5*8
 a + b
 
-# Arrays
-
+# Arrays (vettori)
+# c() è il comando fondamentale per concatenare e crere i vettori
 flowers <- c(3, 6, 8, 10, 15, 18) #senza c() R leggerebbe solo il primo numero, quindi necessito di concatenare i miei dati
 flowers
 
@@ -15,14 +15,17 @@ insects
 
 plot(flowers, insects)
 
-# changing plot paramiters
+# changing plot parameters
 
 # symbols
-plot(flowers, insects, pch=19)
+plot(flowers, insects, pch=19) # pch vuol dire plot character e ti serve per cambiare la fantasia dei simboli
 
 # symbols dimensions
-plot(flowers, insects, pch=19, cex=2)
-plot(flowers, insects, pch=19, cex=.5) #per diminuire la dimensione dei punti devo mettere un numero inferiore ad 1 (posso anche omettere lo 0), è importante che tieni a mente che il separatore è il "."
+plot(flowers, insects, pch=19, cex=2) # cex sta per character exageration
+plot(flowers, insects, pch=19, cex=.5)
+# per diminuire la dimensione dei punti devo mettere un numero inferiore ad 1 (posso anche omettere lo 0).
+# è importante che tieni a mente che il separatore è il "."
 
 # color
 plot(flowers, insects, pch=19, cex=2, col="chocolate1")
+# ricordati che per i colori devi mettere le virgolette 
